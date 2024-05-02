d564e73606ab50f856669cfe2c5538fb95702f3c
zipJiang <tony.jiang.zhengping@gmail.com>
2024-05-02 12:43:25 -0400

    running scoring for different configs

---
+++ runs/score_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+export OUTPUT_PATH=data/outputs/generation_opt.json
+export CACHE_PATH=.cache/.mistral-7b-cache.db
+export SCORE_DIR=data/scores/local_models/
+
+# export SCORE_PATH=${SCORE_DIR}mistral-soft.json
+# conda run -p .env --no-capture-output \
+#     python scripts/run_task.py configs/dedupsoft_configs.yaml \
+#     --cache-path $CACHE_PATH
+
+# export SCORE_PATH=${SCORE_DIR}mistral-factscore.json
+# conda run -p .env --no-capture-output \
+#     python scripts/run_task.py configs/factscore_configs.yaml \
+#     --cache-path $CACHE_PATH
+
+export SCORE_PATH=${SCORE_DIR}gpt3.5-factscore.json
+export CACHE_PATH=.cache/.gpt-3.5-turbo-cache.db
+conda run -p .env --no-capture-output \
+    python scripts/run_task.py configs/factscore_gpt3.5_configs.yaml \
+    --cache-path $CACHE_PATH
