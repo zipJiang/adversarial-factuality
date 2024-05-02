6c408b3cc75458a9442584ad2741ddd5a60d3a93
zipJiang <tony.jiang.zhengping@gmail.com>
2024-05-02 12:43:59 -0400

    loading small scripts for examine wikipedia dump

--- scripts/run_task.py
+++ scripts/run_task.py
@@ -10,14 +10,16 @@ from src.task import Task
 
 @click.command()
 @click.argument("config-path")
+@click.option("--cache-path", default=".cache/.gpt-3.5-turbo-cache.db", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True, readable=True))
 def main(
-    config_path
+    config_path,
+    cache_path
 ):
     """
     """
     
     config = EnvYAML(config_path, flatten=False, env_file="ENV.env", include_environment=True).export()['task']
-    set_llm_cache(SQLiteCache(".cache/.gpt-3.5-turbo-cache.db"))
+    set_llm_cache(SQLiteCache(cache_path))
     Task.from_params(config).run()
     
     
