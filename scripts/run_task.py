3ec6cfada77b6681188ac354cb5cfcf2c9465ad3
Jack Zhang <jzhan237@jhu.edu>
2024-05-13 02:46:21 +0000

    fix

--- scripts/run_task.py
+++ scripts/run_task.py
@@ -11,7 +11,7 @@ from src.task import Task
 @click.command()
 @click.argument("config-path")
 @click.option("--cache-path", default=".cache/.gpt-3.5-turbo-cache.db", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True, readable=True))
-@click.option("--no-cache", action="store_true")
+@click.option("--no-cache", default=True, type=click.BOOL)
 def main(
     config_path,
     cache_path,
