db14dc5125c8d68f1669affbe03ba61e8a6f456a
zipJiang <tony.jiang.zhengping@gmail.com>
2024-05-20 09:37:22 -0400

    remove default caching

--- scripts/run_task.py
+++ scripts/run_task.py
@@ -4,13 +4,13 @@ import click
 from envyaml import EnvYAML
 from registrable import Registrable
 from langchain.globals import set_llm_cache
-from langchain.cache import SQLiteCache
+from langchain_community.cache import SQLiteCache
 from src.task import Task
 
 
 @click.command()
 @click.argument("config-path")
-@click.option("--cache-path", default=".cache/.gpt-3.5-turbo-cache.db", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True, readable=True))
+@click.option("--cache-path", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True, readable=True), default=None)
 def main(
     config_path,
     cache_path
@@ -19,7 +19,8 @@ def main(
     """
     
     config = EnvYAML(config_path, flatten=False, env_file="ENV.env", include_environment=True).export()['task']
-    set_llm_cache(SQLiteCache(cache_path))
+    if cache_path is not None:
+        set_llm_cache(SQLiteCache(cache_path))
     Task.from_params(config).run()
     
     
