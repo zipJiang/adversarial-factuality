141b02867749c7ea786d86294447ccb7e0f30ee4
zipJiang <tony.jiang.zhengping@gmail.com>
2024-05-20 09:37:04 -0400

    adding peft training for SFT

--- src/task/__init__.py
+++ src/task/__init__.py
@@ -6,3 +6,5 @@ from .train_unli_task import TrainUNLITask
 from .calculate_correlation import CalculateCorrelationTask
 from .filter_required_entites import FilterRequiredEntities
 from .sample_datapoints import SampleDatapoints
+from .create_training_examples import CreateTraniningExamples
+from .peft_sft import PeftSFTTask
